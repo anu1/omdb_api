1f888c3d147c35ff11a1f28f5ead16481e8f227f
Anuradha Nakka <nanuradhasarma@gmail.com>
2019-05-28 06:21:32 -0400

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# omdb_api
-OMDB API access
+Repo details:
+
+This repo contains the source code files required to access the Online Movie Database website http://www.omdbapi.com through API call and returns the movie rating if the movie name is found in the database. Sample usage:
+
+Pre-Requisites:
+CentOS 6.x
+python 3.x
+omdb API access, please register on the webpage ('http://omdbapi.com/') and get the API key
+after download: plase replace 'xxxxxx' with the actual api key in the file: omdbapi_keys.json
+[bash@localhost omdbapi]$ cat omdbapi_keys.json 
+{"api":"xxxxxx"}
+
+NOTE: Tested the code under Centos 6.10, python 3.6.8, it may work with latest python versions.
+
+Sample Usage:
+--
+bash$ git clone <repo>
+bash$ cd omdb_api
+[bash@localhost omdbapi]$ python omdb.py  
+missing movie argument
+$ python  omdb.py [-h] <-m movie-name>
+for example: $ python  omdb.py -m "Independence day"
+for example: $ python  omdb.py -m Matrix
+[bash@localhost omdbapi]$ python omdb.py  -h
+Usage: $ python  omdb.py [-h] <-m movie-name>
+[bash@localhost omdbapi]$ python omdb.py  -m joy
+movie name =  joy
+Retrieving the movie data of "joy" now… 
+Rating is 6.6 
+[bash@localhost omdbapi]$ python omdb.py  -m 'the mathematician'
+movie name =  the mathematician
+Retrieving the movie data of "the mathematician" now… 
+Rating is N/A 
+---
+NOTE: If your API key is invalid, you may get an error as follows:
+$ python omdb.py -m test
+movie name =  test
+Retrieving the movie data of "test" now… 
+ERROR: Unauthorized
---
+++ omdb.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+# python code to extract movie rating info from omdb.com webserver
+import urllib.parse
+import urllib.request
+import json
+import pprint
+import sys
+import getopt
+
+# main function to begin the execution
+def main(argv):
+    moviename = ''
+    
+    # check/process the arguments
+    if len(argv) == 0:
+        print("missing movie argument")
+        print("$ python  %s [-h] <-m movie-name>" %sys.argv[0])
+        print("for example: $ python  %s -m \"Independence day\"" %sys.argv[0])
+        print("for example: $ python  %s -m Matrix" %sys.argv[0])
+        sys.exit(2)
+    try:
+       opts, args = getopt.getopt(argv,"hm:",["movie="])
+    except getopt.GetoptError:
+        print("Incorrect arguments")
+        print("Usage: $ python  %s [-h] <-m movie-name>" %sys.argv[0])
+        print("for example: $ python  %s -m \"Independence day\"" %sys.argv[0])
+        print("for example: $ python  %s -m Matrix" %sys.argv[0])
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("Usage: $ python  %s [-h] <-m movie-name>" %sys.argv[0])
+            sys.exit()
+        elif opt in ("-m", "--movie"):
+            moviename = arg
+        else:
+            print("Incorrect arguments")
+            print("Usage: $ python  %s [-h] <-m movie-name>" %sys.argv[0])
+            sys.exit()
+    print(f"movie name =  {moviename}")
+
+    # get the API key value
+    with open('omdbapi_keys.json') as fp:
+        keys = json.load(fp)
+        omdbapi = keys['api']
+
+    omdburl = 'http://www.omdbapi.com/?'
+    apikey = '&apikey='+omdbapi
+
+    # get the movie details
+    get_movie_details(omdburl,apikey,moviename)
+    sys.exit(0)
+
+# function to extract the movie details from omdbapi.com website
+def get_movie_details(omdburl,apikey,title):
+    if len(title) < 1 or title=='quit': 
+        print("exiting...")
+        return None
+    try:
+        url = omdburl + urllib.parse.urlencode({'t': title})+apikey
+        print(f'Retrieving the movie data of "{title}" now… ')
+        uh = urllib.request.urlopen(url)
+        data = uh.read()
+        json_data=json.loads(data)
+ 
+        if json_data['Response']=='True':
+            #pprint.pprint(json_data)
+            print("Rating is %s " %json_data['imdbRating'])
+            #pprint.pprint(json_data)
+            #print(json.dumps(json_data, sort_keys=True))
+
+        else:
+            print(f"Movie \'{title}\' is NOT FOUND, please try again")
+
+    except urllib.error.URLError as e:
+        print(f"ERROR: {e.reason}")
+
+if __name__ == "__main__": main(sys.argv[1:])
+
+exit(0)
